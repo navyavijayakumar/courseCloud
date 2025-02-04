27bdc9d74b48d9cda543472ebf3e055c5d7266dc
Navya Vijayakumar <navyavijayakumar3@gmail.com>
2025-02-04 12:00:27 +0530

    course detail view added

--- student/urls.py
+++ student/urls.py
@@ -3,8 +3,11 @@
 from django.urls import path
 from student import views
 
+
 urlpatterns=[
     path("register/",views.StudentCreateView.as_view(),name="student-register"),
     path("signin/",views.StudentSigninView.as_view(),name="signin"),
-    path("index/",views.IndexView.as_view(),name="index")
+    path("index/",views.IndexView.as_view(),name="index"),
+    path('courses/<int:pk>/',views.CourseDetailView.as_view(),name="course-detail"),
 ]
+
